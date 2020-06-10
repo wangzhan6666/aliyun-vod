aad624781bf48202645cec74b42b8fe207289a7e
zhazhawang <2774891137@qq.com>
2020-06-10 22:00:36 +0800

    根据视频id删除阿里云视频

--- src/main/java/com/sise/wangzhan/controller/VodController.java
+++ src/main/java/com/sise/wangzhan/controller/VodController.java
@@ -1,10 +1,13 @@
 package com.sise.wangzhan.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoResponse;
 import com.sise.wangzhan.service.VodService;
+import com.sise.wangzhan.utils.ConstantPropertiesUtil;
+import com.sise.wangzhan.utils.InitVodClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -28,6 +31,27 @@ public class VodController {
         String vidoeId = vodService.upload(file);
 
         return vidoeId;
+    }
+
+    // 2 根据视频id删除阿里云视频
+    @DeleteMapping("removeAlyVideo/{id}")
+    public String removeAlyVideo(@PathVariable String id) {
+
+        try {
+            // 初始化对象
+            DefaultAcsClient client = InitVodClient.initVodClient(ConstantPropertiesUtil.KEY_ID, ConstantPropertiesUtil.KEY_SCRET);
+            // 删除视频的request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+            // 向request中设置id
+            request.setVideoIds(id);
+            // 调用初始化对象的方法实现删除
+            client.getAcsResponse(request);
+
+            return "删除成功";
+        }catch (Exception e) {
+            e.printStackTrace();
+            return "删除失败";
+        }
     }
 
 }
